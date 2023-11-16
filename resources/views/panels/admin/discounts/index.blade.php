6da91d1693b40319f2ae251c165269cfefe1e0a6
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-11-17 01:29:29 +0330

    chore: delete edit href a tag from discounts

--- resources/views/panels/admin/discounts/index.blade.php
+++ resources/views/panels/admin/discounts/index.blade.php
@@ -51,9 +51,6 @@
                                                 {{ __('Delete') }}
                                             </x-danger-button>
                                         </form>
-                                        <a class="inline-flex items-center px-4 py-2 bg-yellow-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-500 active:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellowed-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" href="{{ route('discounts.edit', $discount) }}">
-                                            {{ __('Edit') }}
-                                        </a>
                                     </div>
                                 </td>
                             </tr>
