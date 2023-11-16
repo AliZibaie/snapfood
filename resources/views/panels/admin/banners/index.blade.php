5cfe8f8755559f5f377808732ffb2185d140206d
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-11-16 16:53:35 +0330

    chore: implement messages status of actions

--- resources/views/panels/admin/banners/index.blade.php
+++ resources/views/panels/admin/banners/index.blade.php
@@ -15,6 +15,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    @if(session('success'))
+                        <p class="text-xl text-center text-green-700">{{session('success')}}</p>
+                    @else
+                        <p class="text-xl text-center text-red-700">{{session('fail')}}</p>
+
+                    @endif
                     <table class="table-fixed w-full">
                         <thead>
                         <tr>
