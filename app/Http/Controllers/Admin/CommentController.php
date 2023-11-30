d416e85500109e43abbea8cf1b2367cfd382edb0
AliZibaie <130165281+AliZibaie@users.noreply.github.com>
2023-11-30 22:13:03 +0330

    feat: implement ability of deleting comments that sellers requested for admin

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use App\Services\Admin\Comment as Service;
 
@@ -12,6 +13,17 @@ class CommentController extends Controller
     {
         $comments = Service::index();
         return view('panels.admin.comments.index', compact('comments'));
+    }
+
+    public function destroy(int $id)
+    {
+        try {
+            $comment = Comment::query()->find($id);
+            $comment->delete();
+            return redirect('panel/comments/requests')->with('success', 'comment deleted successfully!');
+        }catch (\Throwable $exception){
+            return redirect('panel/comments/requests', 500)->with('fail', 'failed to delete comment!');
+        }
     }
 
 }
--- app/Services/Admin/Comment.php
+++ app/Services/Admin/Comment.php
@@ -14,6 +14,7 @@ class Comment
                 ->join('food', 'orders.food_id', '=', 'food.id')
                 ->select('comments.*')
                 ->where('comments.seller_wants_delete', 1)
+                ->whereNull('comments.deleted_at')
                 ->get()
                 ->sortDesc();
 
